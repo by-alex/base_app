14895fd50ca02449056d89f1b3c35d641eac1e36
by-alex <dv.by.alex@gmail.com>
2012-10-20 20:39:39 -0700

    c1

---
+++ base_app/src/com/by_alex/base_app/mai.java
@@ -0,0 +1,15 @@
+package com.by_alex.base_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class mai extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
